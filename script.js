007e37665874994441d6bde69839a64bc76be9a0
Unknown <justinkjetton@gmail.com>
2017-12-03 15:30:28 -0600

    It all works

--- script.js
+++ script.js
@@ -3,3 +3,4 @@ var userText = document.getElementById('userInput').value;
 function printResults() {
   var userText = document.getElementById('userInput').value;
   document.getElementById("result").innerHTML = userText;
+}
