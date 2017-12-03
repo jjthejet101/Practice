1328377851ba02f8ff9638f4ad47eb7113dc1166
Unknown <justinkjetton@gmail.com>
2017-12-03 15:27:25 -0600

    Beginning

---
+++ script.js
@@ -0,0 +1,5 @@
+var userText = document.getElementById('userInput').value;
+
+function printResults() {
+  var userText = document.getElementById('userInput').value;
+  document.getElementById("result").innerHTML = userText;
